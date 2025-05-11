c290f9dc644c39ede7bec9f47a98ed916023c5f8
Shantanu Ingle <147296969+shantanu-ingle@users.noreply.github.com>
2025-05-11 10:43:57 +0530

    Initial commit with Spring Boot project

---
+++ src/main/java/com/example/HonorsProject/HonorsProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.HonorsProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HonorsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HonorsProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/HonorsProject/HonorsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HonorsProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HonorsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
